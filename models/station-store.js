42d12dbb30a305f152d034dc07b00129a2b91faf
Glitch (happy-fate-laugh) <none>
2022-08-17 23:47:44 +0000

    🛣🏛 Checkpoint ./models/station-store.json:47148414/4680 ./models/station-store.js:47148414/948

--- models/station-store.js
+++ models/station-store.js
@@ -1,49 +1,5 @@
-"use strict";
+'use strict';
 
-const logger = require("../utils/logger");
-
-const station = {
-  name: "Tramore",
-  readings: [
-    {
-      code: "800",
-      temp: "0.5",
-      windspeed: "3.5",
-      pressure: "1002",
-    },
-    {
-      code: "600",
-      temp: "6.0",
-      windspeed: "2.0",
-      pressure: "1004",
-    },
-    {
-      code: "700",
-      temp: "8.0",
-      windspeed: "1.0",
-      pressure: "1005",
-    },
-  ],
-};
-
-const station2 = {
-  name: "Dunmore",
-  readings: [
-    {
-      code: "700",
-      temp: "8.0",
-      windspeed: "1.0",
-      pressure: "999",
-    },
-    {
-      code: "200",
-      temp: "0.5",
-      windspeed: "3.5",
-      pressure: "1000",
-    },
-  ],
-};
-
-const stationCollection = [station, station2];
+const stationCollection = require('./station-store.json').stationCollection;
 
 module.exports = stationCollection;
